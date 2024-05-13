1e0b5d4d097a369b21185370b29531a579729682
Tugce BULUT <168874289+TugceHub@users.noreply.github.com>
2024-05-13 04:08:54 +0300

    donguler eklendi

--- Program.cs
+++ Program.cs
@@ -1,297 +1,158 @@
-﻿
+﻿// DÖNGÜLER
 
-// KARAR YAPILARI:
+//Belirtişen işlemlerin belli şartlar aralığında tekrar tekrar gerçekleşmesidir.
+// ornegün 100 defa merhaba dünya yazdırmak gibi
+// 1000 tane personel için numara atamak
 
-// Temelde iki komutu vardır if - else
+/* DÖNGÜ TÜRLERİ
+ *  1. FOR
+ *  2. WHILE
+ *  3. DO-WHILE
+ *  4. FOREACH 
+  */
 
-/*syntax yapısı: 
-    if (şart) {
-     işlem 1
-    }else
-     işlem 2
-    }
-*/
-// OPERATÖRLER
-/*
- * =  : atama 
- * == : eşit mi ?
- * >  : büyük mü ?
- * <  : küçük mü ?
- * != : eşit değilse
- * &  : ve
- * |  : veya
- * %  : mood (tek mi - çift mi )
+/* For Dongüsü syntax yapısı
+ *  for( başlangıç; bitiş; miktar)
+ *             {
+ *         işlemler
+ *             }
  */
 
-// IF - ELSE KULLANIMI 
+// Örnek For Döngüsü Kullanımı 1
 /*
-string sehir;
-Console.WriteLine("Şehir adı: ");
-sehir = Console.ReadLine();
-if (sehir == "adana") // klavyedne de anı fontla girilmeli yoksa hata verir
+int i;
+for(i=1; i<10; i++)
 {
-    Console.WriteLine("doğru şehir");
-}
-else
-{
-    Console.WriteLine("yanlış şehir");
-}
-*/
+    Console.WriteLine("merhaba dünya");
+} */
+// BREAK POINT KULLANIMI
+// break point  gri kısıma tıkadığında kırmızı nokta oluştururç
+// kullanım amacı hata aldığım noktalarda programı adım adım takip etmemi sağlar.
+// f5 ile çalıştır sonra f11 ile takip et
 
-//Öğrenci Sınav-Not Uygulaması
-/*
-int sayi;
-Console.WriteLine("Syı giriniz: ");
-sayi = Convert.ToInt16(Console.ReadLine());
+// Örnek For Döngüsü Kullanımı 2
 
-if (sayi == 23)
-{
-    Console.WriteLine("SAYI DOĞRU");
-}
-else
-{
-    Console.WriteLine(" sayı yanlış");
-}
+// EKRANA 1-10 KADAR SAYI YAZDIR.
 
+//int sayi;
+//for (sayi = 1; sayi <= 10; sayi++)
+//{
+//    Console.WriteLine(sayi);
+//}
 
-int s1, s2, s3, ort;
-Console.WriteLine(" 1. sınav notunuzu giriniz");
-s1 = Convert.ToInt32(Console.ReadLine());
+// 0 ile 20 arası çift sayılar
 
-Console.WriteLine(" 2. sınav notunuzu giriniz");
-s2 = Convert.ToInt32(Console.ReadLine());
+//int j;
+//for ( j = 0; j <=20; j+=2 )
+//{
+//    Console.WriteLine( j ); 
+//}
 
-Console.WriteLine(" 3.  sınav notunuzu giriniz");
-s3 = Convert.ToInt32(Console.ReadLine());
+//int k;
+//for ( k = 1; k <= 100; k += 1)
+//{
+//    Console.WriteLine(k +"Merhaba İstanbul");
+//}
 
-ort = (s1 + s2 + s3) / 3;
-Console.WriteLine( "Ortalamanız: "+ort);
+// Döngü İçinde Karar Yapısı Kullanımı
 
-if (ort >= 50)
-{
-    Console.WriteLine("DERSİ GEÇTİNİZ!");
-}
-else {
-    Console.WriteLine("DERSİ GEÇEMEDİNİZ");
-}
+// 0 ile 20 arası çift sayılar
 
-*/
+//int sayi, cift;
+//for (sayi = 0; sayi <= 20; sayi++) {
+//    cift = sayi% 2;
+//    if (cift == 0) {
+//        Console.WriteLine(sayi);
+//    }
+//}
 
-// Karar Yapıları ile Ve-Veya Operatörlerinin Kullanımı
-/*
-string kullanıcı, sifre;
-Console.WriteLine("kullanıcı adınız: ");
-kullanıcı = Console.ReadLine();
-Console.WriteLine(" şifreniz: ");
-sifre = Console.ReadLine();
+// Algoritmik Soru Girilen Sayının Pozitif Tam Bölenlerini Bulma
 
-if (kullanıcı == "Freya" & sifre == "12456")
-{
-    Console.WriteLine(" WELCOME ");
-}
-else
-{
-    Console.WriteLine("HATA");
-}
-*/
+//int sayi;
+//Console.WriteLine("sayıyı giriniz :");
+//sayi = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine();
+//Console.WriteLine("Sayının Tam bölenleri :");
+//for (int i = 1; i <=sayi; i++)
+//{
+//    if ( sayi % i == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
 
-// Birden Fazla if Bloğu Kullanımı
+// Algoritmik Mülakat Sorusu
 
-/*
-int s1, s2, s3, ort;
-Console.WriteLine(" Sınav 1: ");
-s1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(" Sınav 2: ");
-s2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Sınav 3:");
-s3 = Convert.ToInt32(Console.ReadLine());
-ort = (s1 + s2 + s3) / 3;
-Console.WriteLine("Ortalamanız :" +ort);
-Console.WriteLine("");
+/* Bir bakteri türü her saatin sonunda kendini 2 ye bölerek çoğalmaktadır.
+ * Yeni oluşan bakterilerde aynı şekilde çoğalmaktadır.
+ * 24 saatin sonunda kaç bakteri olur?  */
 
-if (ort <= 0)
-{
+//int bakteri = 1;
+//for (int i = 1 ; i <= 24; i++) {
+//    bakteri = bakteri * 2;
+//    Console.Write("SAAT :" + " " + i);
+//    Console.WriteLine(bakteri);
 
-    Console.WriteLine(" HATA : Ortalamanız 0 dan küçük olamaz ");
-    
-}
-if (ort>= 0 & ort < 50)
-{
-    Console.WriteLine(" Başarısız ");
-}
-if (ort >= 50 & ort < 65)
-{
-    Console.WriteLine(" Geçer");
+//}
+//Console.Write("24 SAATİN SONUNDA OLUŞAN BAKTERİ SAYISI : ");
+//Console.WriteLine(bakteri);
 
-}
-if (ort > 65 & ort <= 70)
-{
-    Console.WriteLine(" Orta ");
-}
-if (ort > 70 &  ort < 85)
-{
-    Console.WriteLine(" iyi ");
+// Ardışık Sayılarla İşlemler
 
-}
-if (ort >= 85 & ort <= 100)
-{
-    Console.WriteLine(" Başarılı ");
-}
-if(ort > 100)
-{
-    Console.WriteLine(" HATA : Ortalamanız 100 den büyük olamaz");
-}
+// 5 Faktöriyel 
+//int faktoriyel = 1;
+//for (int i = 5; i >= 0; i--)
+//{
+//    faktoriyel = faktoriyel * i;
+//    //Console.WriteLine(faktoriyel); -> aşamaları gosterir
+//}
+//Console.WriteLine(faktoriyel);  // sonucu gosterir.
 
-*/
-
-// "Eşit Değilse" Operatörü ile Karar Yapıları
-
-/*
-char karakter;
-Console.WriteLine("Karakteri giriniz : ");
-karakter = Convert.ToChar(Console.ReadLine());
-Console.WriteLine();
-if (karakter != '*')
-{
-    Console.WriteLine("Hatalı giriş yaptınız !");
-}
-else
-{
-    Console.WriteLine(" HOŞ GELDİNİZ ! ");
-}
-    */
-
-// Bilgi Yarışması Projesi - 1
-/* KURALLAR :
- *  TOPLAM SORU SAYISI : 3
- *  HER SORU 4 ŞIK
- *  DİĞER SORUYA GEÇE BİLMEK İÇİN DOĞRU CEVAP VERMEK GEREKLİ 
- *  YANLIŞ CEVAPTA YARIŞMA SONA ERER
- *  3 SORUNUN TAMAMI DOĞRU İSE YARIŞMA BİTER.
+/* odev
+ * klavyeden girilen sayının faktoriyelini hesaplayan uygulama
+ * dongunun başlangıc değerleri buyukten kucuge değl de kucukten buyuğe olmalı.
  */
-/*
-Console.WriteLine("Bilgi Yarışması");
-Console.WriteLine();
-Console.WriteLine();
-int soru = 1;
-string cevap;
-if (soru == 1)
-{
-    Console.WriteLine("Mustafa Kemal Atatürk kaç yılında doğmuştur ?");
-    Console.WriteLine();
-    Console.WriteLine(" A) 1998");
-    Console.WriteLine(" B) 1881");
-    Console.WriteLine(" C) 1919");
-    Console.WriteLine(" D) 1923 ");
-    Console.WriteLine();
-    Console.Write(" Cevap :");
-    cevap = Console.ReadLine();
-    Console.WriteLine(); 
 
-    if (cevap == "b" | cevap == "B")
-    {
-     // Console.WriteLine(" DOĞRU Cevap");
-     soru = soru + 1;
+// While Döngüsü
 
-    }
-    else
-    {
-        Console.WriteLine(" Yanlış Cevap OYUN BİTTİ puan: 0");
-    }
-}
-if (soru == 2)
-{
-    Console.WriteLine("Mustafa Kemal Atatürk milli mücadele için Samsun'a kaç yılında çıkmıştır?");
-    Console.WriteLine();
-    Console.WriteLine(" A) 1998");
-    Console.WriteLine(" B) 1881");
-    Console.WriteLine(" C) 1919");
-    Console.WriteLine(" D) 1923 ");
-    Console.WriteLine();
-    Console.Write(" Cevap :");
-    cevap = Console.ReadLine();
-    Console.WriteLine(); 
+// syntax yapısı:
+/*      While(şart)
+ *          {
+ *       işlemler
+ *          }
+ */
+// Özellikle veri tabanından veri çekmede çokca kullanılan bir özelliktir.
 
-    if (cevap == "c" | cevap == "C")
-    {
-        // Console.WriteLine(" DOĞRU Cevap");
-        soru = soru + 1;
+//int sayac = 1;
+//while (sayac <= 10)
+//{
+//    Console.WriteLine("Fatma Tugce BULUT");
+//    sayac++;
+//}
 
-    }
-    else
-    {
-        Console.WriteLine(" Yanlış Cevap OYUN BİTTİ puan :10 ");
-    }
-}
-if (soru == 3)
-{
-    Console.WriteLine("Türkiye Cumhuriyeti kaç yılında kurulmuştur?");
-    Console.WriteLine();
-    Console.WriteLine(" A) 1998");
-    Console.WriteLine(" B) 1881");
-    Console.WriteLine(" C) 1919");
-    Console.WriteLine(" D) 1923 ");
-    Console.WriteLine();
-    Console.Write(" Cevap :");
-    cevap = Console.ReadLine();
-    Console.WriteLine(); 
+// Do While Döngüsü
+// While döngüsünde şart gerçekleşmezse çıktı alınmaz ancak dı whşle da 1 kere de olsa şart gerçekleşir.
 
-    if (cevap == "d" | cevap == "D")
-    {
-        Console.WriteLine(" Tebrikler Tüm Cevaplar DOĞRU ");
+// Do - While döngüsü syntax yapısı:
+/*      Do
+ *      {
+ *      işlemler
+ *      }
+ *      While( Şart ); // şart sağlanamzsa işlemden çıkıcak bu yüzden ";" önemli.
+ */
 
-    }
-    else
-    {
-        Console.WriteLine(" Yanlış Cevap OYUN BİTTİ ");
-    }
-}
-*/
+//int sayi;
+//sayi = 1;
+//while (sayi <= 10)
+//{
+//    Console.WriteLine(sayi);
+//    sayi++;
+//}
 
-// Switch Case Kullanımı
+//int sayi = 12;
+//do
+//{
+//    Console.WriteLine(sayi);
+//    sayi++;    
 
-// Dallanmanın fazla olduğu durumlarda kullanılan karar yapısı alt başlığıdır.
-
-/* switch (Deger)
-            {
-         Case1: işlemler
-            Break;
-             ...
-        Default : işlemler
-            Break;
-              }
-*/
-
-
-//  Switch Case ile Plaka-Şehir Uygulaması
-/*
-byte plaka;
-Console.WriteLine(" Lütfen plaka giriniz: ");
-plaka = byte.Parse(Console.ReadLine());
-switch (plaka)
-{
-    case 1:Console.WriteLine("Merhaba ADANA "); break;
-    case 2: Console.WriteLine("Merhaba ADIYAMAN"); break ;
-    case 3: Console.WriteLine("Merhaba AFYON"); break;
-    default: Console.WriteLine(" Henüz Bu Şehir Bilgisi Girilmedi "); break;
-}
-*/
-
-// Switch Case ile String Değişkenlerin Kullanımı
-/*
-Console.WriteLine(" String Değişkenler ile Switch Case Kullanımı");
-Console.WriteLine();
-string mevsim;
-Console.WriteLine("Lütfen mevsim giriniz : ");
-mevsim = Console.ReadLine();
-switch (mevsim)
-{
-    case "kis": Console.WriteLine("Aralık - Ocak - Şubat"); break;
-    case "ilkbahar": Console.WriteLine("Mart - Nisan - Mayıs");break;
-    case "yaz": Console.WriteLine("Haziran - Temmuz - Ağustos"); break;
-    case "sonbahar":Console.WriteLine(" Eylül - Ekim - Kasım "); break;
-   
-
-    default:Console.WriteLine("hatalı mevsim girişi");break; 
-}
-*/
+//} while ( sayi <= 10);
