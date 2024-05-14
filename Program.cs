ae9d637122f6a0ce94daa6c9df986e02b4fa3392
Tugce BULUT <168874289+TugceHub@users.noreply.github.com>
2024-05-15 02:36:42 +0300

    Hazır fonsiyonlar eklendi

--- Program.cs
+++ Program.cs
@@ -1,176 +1,89 @@
-﻿// Sınıflar ve Nesneler
-// Nesne Nedir?
-//    SınıfAdı NesneAdı = new SınıfAdı();
-
-// Uygulama: Araba Sınıfı
-
-using sınıflarVeNesneler;
-using System.Runtime.InteropServices;
-
-//class program
-//{
-//    static void Main(string[] args)
-//    {
-//        Araba ar = new Araba();
-//        ar.marka = "ferrai";
-//        ar.hiz = 200;
-//        ar.renk = "lacivert";
-//        ar.motor = 2.0;
-
-//        Müşteri mu = new Müşteri();
-//        mu.id = 1881;
-//        mu.name = "Fatma ";
-//        mu.surname = "BULUT";
-//        mu.city = "Ankara";
-
-//        Console.WriteLine("*********** ARABA TANTIM KARTI 1 ************");
-//        Console.WriteLine();
-//        Console.WriteLine("Marka : " + ar.marka);
-//        Console.WriteLine("Hız : " + ar.hiz);
-//        Console.WriteLine("Renk : " + ar.renk);
-//        Console.WriteLine("Motor : " + ar.motor);
-
-//        Console.WriteLine("*********** Müşteri Bilgisi ************");
-//        Console.WriteLine();
-//        Console.WriteLine("Ad Soyad : " + mu.id +" - "+ mu.name + mu.surname);
-//        Console.WriteLine("Şehir : " + mu.city);
-//        Console.WriteLine();
-//        Console.WriteLine();
-
-//        mu.id = 1938;
-//        mu.name = "Beyza Basak ";
-//        mu.surname = "BULUT";
-//        mu.city = "Ankara";
-//        Console.WriteLine("Ad Soyad : " + mu.id + " - " + mu.name + mu.surname);
-//        Console.WriteLine("Şehir : " + mu.city);
-
-//    }
-
-//}
-
-//Sınıf İçindeki Niteliklere Klavyeden Değer Girişi
-
-//class Program
-//{
-//    static void Main(String[] args)
-//    {
-//        Müşteri m = new Müşteri();
-
-//        Console.WriteLine("ID : ");
-//        m.id = Convert.ToInt16(Console.ReadLine()); // int olduğu için convertledik
-
-//        Console.WriteLine("AD : ");
-//        m.name = Console.ReadLine();
-
-//        Console.WriteLine("SOYAD : ");
-//        m.surname = Console.ReadLine();
-
-//        Console.WriteLine("Şehir : ");
-//        m.city = Console.ReadLine();
-//    }
-//}
-
-// Kapsülleme Nedir?
+﻿// Matematiksel Fonksiyonlar
 /*
- * Nesnelerin niteliklerinin bilinçsiz kullanımını önlemek.
- * İlgili özelliklerin istenilen değer ya da aralıkta olmasını sağlamak.
- * Değişkenlere direkt erişimin önüne geçmek.
- * Arka planda kodları gizlemek.
- *** Anahtar kelime: property
+ * Abs : Mutlak
+ * Ceiling : Üst tabana yuvarla
+ * Floor   : Alt tabana yuvarla 
+ * Sqrt    : Karekök
+ * Pow     : Üs alma 
+ * Pi      : Pi sayısı
+ * */
+
+// Math :
+//double sayi;
+//Console.WriteLine("Sayıyı giriniz :");
+//sayi = Convert.ToDouble(Console.ReadLine());
+
+//// Mutlak Değer:
+//Console.WriteLine("Mutlak Değer :" +Math.Abs(sayi));
+//// Üst taban :
+//Console.WriteLine("Üst Taban : " + Math.Ceiling(sayi));
+//// Alt Taban :
+//Console.WriteLine("Alt Taban : " + Math.Floor(sayi));
+//// Kare Kök :
+//Console.WriteLine("Karekök : " + Math.Sqrt(sayi));
+
+//String Fonksiyonlar
+/*
+ * Metinsel ifadeler ile işlemler yapabilmek için kullanlır
+ * Conctac    : birleştime
+ * Length     : uzunluk
+ * İndex of   : Metin arama
+ * Starswith   : İlgili parametere ile mi başlar.
+ * Toupper    : büyük harf
+ * Tolower    : küçük harf
+ * Remove     : İstanilen karekterden itibaren siler
+ * Replace    : Değiştirme
+ * Substring  : İstaneilen karakterden itibaren işlem yapar 
+ * Trim       : Sağ - sol boşlukları kaldır.
  */
 
-//Kapsülleme Örneği
+// String Fonksiyon Kullanımları 1
 
-//class Program
-//{
-//    static void Main(String[] args)
-//    {
-//        Müşteri m = new Müşteri();
+//string metin, metin2;
+//Console.WriteLine("Metini giriniz : ");
+//metin = Console.ReadLine();
+//Console.WriteLine("Metini giriniz : ");
+//metin2 = Console.ReadLine();
 
-//        Console.WriteLine("ID : ");
-//        m.id = Convert.ToInt16(Console.ReadLine()); // int olduğu için convertledik
+//Console.WriteLine("Concat ile birleştirme : " + string.Concat(metin, metin2));
+//Console.WriteLine("Metin 1 için karakter sayısı : " + metin.Length);
+//Console.WriteLine("İndex of Örneği : " + metin.IndexOf("aydın")); // index 0 ise mevcut || - değer döndürür.
+//Console.WriteLine("Startswith Örneği : " + metin.StartsWith("o"));
+//Console.WriteLine("Merhaba Dünya" + metin.Trim());
+//Console.WriteLine("Büyük harf : " + metin.ToUpper());
+//Console.WriteLine("Küçük harf : " + metin.ToLower());
+//Console.WriteLine("Remove Metodu  : " + metin.Remove(5)); // index numasından sonrasını siler.
+//Console.WriteLine("Replace Fonksiyonu  : " + metin.Replace("a", "A")); // eski karakter, yeni karakter
+//Console.WriteLine("Substring Fonksiyonu : " + metin.Substring(4)); // indeks numarasından sonrasın yazar  indeks 
 
-//        Console.WriteLine("AD : ");
-//        m.Name = Console.ReadLine();
-
-//        Console.WriteLine("SOYAD : ");
-//        m.Surname = Console.ReadLine();
-
-//        Console.WriteLine("Şehir : ");
-//        m.city = Console.ReadLine();
-
-//        Console.WriteLine("************* MÜŞTERİ BİLGİSİ ********************");
-//        Console.WriteLine(" ");
-//        Console.WriteLine("Kayıt BAŞARILI : "+ m.id+" - " +m.Name+" " + m.Surname+" " + m.city );
-//    }   
-
-//}
-
-// KALITIM NEDİR ?
-// Projelerimizi gerçekleştririken bir sınıfa ait değişkenlerin bir kaçını ya da tamamaını başka bir sınıf içerinde kullanmaktır.
-
-// Kalıtım Kod Örneği
-
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        papagan p   = new papagan();
-//        p.tur = "Papağan";
-//        p.hız = 50;
-//        p.isim = "sultan";
-//        p.ses = "cik";
-//        p.renk = "sarı - kırmızı";
-//        p.kg = 1.650;
-
-//        Console.WriteLine("Tür : " + p.tur);
-//        Console.WriteLine("İsim : " + p.isim);
-//        Console.WriteLine("Hız : " + p.hız);
-//        Console.WriteLine("KG : " + p.kg);
-//        Console.WriteLine("Ses : " + p.ses);
-
-//    }
-//}
+// Tarih Zaman Fonksiyonlar
+/*
+ * Anahtar kelime : Datetime
+ * Kullanım Şekli : DateTime.Now
+ * Day, mounth, year, hour, second, ...vb.
+ * Timespan : 2 tarih arasındaki farkı hesaplar.
+ */
 
 
-// Çok Biçimlilik Nedir?
-/* Miras alma işleminden sonra herhangi bir alanda bulunan değerleri miras alınan sınıftaki haliyle değil de bizim istediğimiz formatta kullanım sağlamasıdır.
-   * Virtual: Kalıtım alınan sınıflarda içeriğin değiştirilebilmesi için kullanılan komutlardır.
-   *Override: Geçersiz kılma anlamına gelen bu komut, değiştirilecek metot yazılmadan önce metotun başına eklenir.
-*/
+//Console.WriteLine("Bugünün gün bilgisi : " + DateTime.Now.Day);
+//Console.WriteLine("Bugünün ay bilgisi : " + DateTime.Now.Month);
+//Console.WriteLine("Bugünün yıl bilgisi : " + DateTime.Now.Year);
+//Console.WriteLine("Bugünün saat bilgisi : " + DateTime.Now.Hour);
+//Console.WriteLine("Bugünün dk bilgisi : " + DateTime.Now.Minute);
+//Console.WriteLine("Bugünün sn bilgisi : " + DateTime.Now.Second);
+//Console.WriteLine("Bugünün kısa  bilgisi : " + DateTime.Now.ToShortDateString());
+//Console.WriteLine("Bugünün uzun  bilgisi : " + DateTime.Now.ToLongDateString());
 
-// Çok Biçimlilik Kod Uygulaması
+// Time Span Kullanımı
+
+//TimeSpan zaman;
+//int gunfarki;
+//DateTime tarih1, tarih2;
+//tarih1 = Convert.ToDateTime("20.08.1997");
+//tarih2 = DateTime.Now.Date;
+//zaman = tarih2 - tarih1;
+//gunfarki = zaman.Days;
+//Console.WriteLine(" Fark :" + gunfarki);
 
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        papagan p = new papagan();
-        p.tur = "Papağan";
-        p.hız = 50;
-        p.isim = "sultan";      
-        p.renk = "sarı - kırmızı";
-        p.kg = 1.650;
-        p.sescikar();
 
-        Console.WriteLine("Tür : " + p.tur);
-        Console.WriteLine("İsim : " + p.isim);
-        Console.WriteLine("Hız : " + p.hız);
-        Console.WriteLine("KG : " + p.kg);
-        Console.WriteLine("Ses : " + p.sescikar());
-
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine("********************");
-        Console.WriteLine();
-        karga k = new karga();
-        k.kg = 1.2 ;
-        k.hız = 80;
-        k.isim = "karga";
-        k.renk = "siyah";
-        k.sescikar();
-
-        Console.WriteLine("KG : " + k.kg + " " + "Hız : "+ k.hız + " " + "İsim : "+ k.isim + " "+ "Renk : "+ k.renk+" " +"Ses : "+ k.sescikar());
-    }
-}
