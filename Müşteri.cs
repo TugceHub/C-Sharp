1fafc153ba2bc8c4644109eb3ddfcdf464a0761f
Tugce BULUT <168874289+TugceHub@users.noreply.github.com>
2024-05-14 20:01:00 +0300

    kapsülleme yapıldı

--- Müşteri.cs
+++ Müşteri.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//namespace sınıflarVeNesneler
+//{
+//    internal class Müşteri
+//    {
+//        public int id;
+//        public string name;
+//        public string surname;
+//        public string city;
+//    }
+//}
+
+// Kapsülleme 
+
 namespace sınıflarVeNesneler
 {
     internal class Müşteri
     {
         public int id;
-        public string name;
+        private string name;
         public string surname;
         public string city;
+
+        public string Name { get => name; set => name = value.ToLower(); }
+        public string Surname { 
+            get => surname; 
+            set => surname = value.ToUpper(); }
+
+        /* CTRL + R +R  -> APPLY 
+         * Bir harfi büyük olmalı 
+         */
+
     }
 }
